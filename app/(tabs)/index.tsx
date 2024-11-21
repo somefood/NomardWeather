09fa2cc053de2919866b4cc5cc5d8b80f33dade6
Seokju Hong <iamseokjuhong@gmail.com>
2024-11-22 00:27:24 +0900

    https://docs.expo.dev/tutorial/add-navigation/ 실습

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import {Link} from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home screen</Text>
+      <Link href="/app/(tabs)/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -18,5 +22,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
-  }
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
 })
